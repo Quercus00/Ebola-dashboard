9c9a70addeff67a8d7efe7caacc6fe6fb3047328
Antoine <antoine.didier@efrei.net>
2020-12-06 20:03:59 +0100

    ajout images

--- ProjetPython/test.py
+++ ProjetPython/test.py
@@ -16,15 +16,30 @@ page = st.sidebar.radio("",('Accueil', "La maladie d'Ebola", 'Présentation du p
 if(page == "Accueil"):
     st.title("DIDIER Antoine - DINDART Juliette")
     st.header("Ebola - dashboard")
-    st.write("Ce projet est de créer un dashboard sur un dataset en streamlit")
+    st.image("streamlit_logo.png")
+    st.write("Ce projet est de créer un dashboard sur un dataset en streamlit. L'objectif est d'analyser le dataset et de comprendre comment l'épidémie s'est propagée en Afrique et en Europe ainsi que de voir comment la communauté internationale a pu gérer cette crise.")
+    st.write("Lien de notre Github : https://github.com/Quercus00/Ebola-dashboard")
+
 elif(page =="La maladie d'Ebola"):
     st.title("Ebola - Présentation")
     st.write("Le virus Ebola est l'agent infectieux qui provoque, chez l'humain et les autres primates, des fièvres souvent hémorragiques — la maladie à virus Ebola — à l'origine d'épidémies historiques notables par leur ampleur et leur sévérité. La transmission entre humains a lieu avant tout par contact direct avec des fluides corporels.")
-elif(page == "Présentation du projet"):
-    st.write("kyaaa")
+    st.write("La maladie qu'il engendre, pour laquelle il n'existe pas jusqu'ici de traitement homologué, présente un taux de létalité allant de 25 % à 90 % chez l'humain ; l'épidémie qui a sévi en Afrique de l'Ouest en 2014 et 2015 affichait ainsi une létalité de 39,5 % au 27 mars 2016, avec 11 323 morts sur 28 646 cas recensés.Après un test efficace en 2015 lors d'une épidémie en Guinée, un premier vaccin a été annoncé à la fin de 2016 et utilisé pour une campagne vaccinale en Afrique de l'Ouest en 2017 ainsi qu’en République démocratique du Congo en 2019")
+    st.header("Les symptômes")
+    st.write("Cette image ci-dessous représente les symptômes distinctifs d'une personne contaminée par le virus Ebola.")
+    st.image("symptomes.jpg")
 
-    # st.bar_chart(df['Cumulative no. of confirmed, probable and suspected cases'])
-    # st.bar_chart(df['Cumulative no. of confirmed, probable and suspected deaths'])
+elif(page == "Présentation du projet"):
+
+    st.title("Présentation du projet")
+
+    st.header("Affichage des cas et morts cummulés")
+    st.write("Nombre cummulé des cas d'Ebola")
+    st.bar_chart(df['Cumulative no. of confirmed, probable and suspected cases'])
+    st.write("Nombre cummulé des morts d'Ebola")
+    st.bar_chart(df['Cumulative no. of confirmed, probable and suspected deaths'])
+
+    st.write("")
+
 
     test = df
     output = test.groupby(["Date"])["Cumulative no. of confirmed, probable and suspected cases"].sum()
@@ -93,6 +108,7 @@ elif(page == "Présentation du projet"):
         highlight=True,
     ).add_to(m)
 
+    """"
     NIL = folium.features.GeoJson(
         map_df,
         style_function=style_function,
@@ -107,6 +123,7 @@ elif(page == "Présentation du projet"):
     m.add_child(NIL)
     m.keep_in_front(NIL)
     folium.LayerControl().add_to(m)
+    """
 
     # call to render Folium map in Streamlit
     folium_static(m)
